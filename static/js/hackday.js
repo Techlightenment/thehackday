31dd4331f4674a09b6dfa73096fa3648901af2ba
Luke Ashworth <luke@techlightenment.com>
2012-05-31 11:55:38 +0100

    test

--- static/js/hackday.js
+++ static/js/hackday.js
@@ -4,46 +4,65 @@ $(document).ready(function(){
   
   function Socket(parent, word, endpoint) {
     //biggraph
+    var self = this;
     this.parent = parent;
     this.endpoint = endpoint;
     this.root = "ws://" + location.host + "/" + endpoint + "?hashtag=" + word;
     
     this.socket = new WebSocket(this.root);
-    
+    console.log(this.root)
     this.socket.onmessage = function(e){
+      console.log("updating " + word)
+
       this.update(e.data);
-    }
+    }.bind(this)
   }
   //5333
   Socket.prototype.update = function(data){
-    var maxPoints = 400;
+    var maxPoints = 400,
+        t = new Date().getTime(),
+        data = JSON.parse(data),
+        flot = this.parent.flot;
     // Munge data her to current flot data
     // Set max number of points
-    var cdata = this.parent.flot.getData();
-    
+    var cdata = flot.getData();
+      
     _.each(cdata, function(d,i){
-      var newPoint = [data.time, data.data[i]];
+      var newPoint = [t, data[i]];
       
-      d.concat(newPoint)
+      d.data.push(newPoint);
       
-      if(d.length > maxPoints){
-        d.slice(0, maxPoints - d.length);
+      if(d.data.length > maxPoints){
+        d.data.slice(0, maxPoints - d.data.length);
       }
-      
-      this.parent.flot.update();
-      this.parent.flot.draw();
     });
+    
+    flot.setData(cdata);
+    flot.setupGrid();
+    flot.draw();
+    var start = cdata[0].data[0][0];
+    
+    var range = Math.round(((t - start) / 1000));
+    
+    this.parent.target.find('.timerange').html('In the last ' + range + ' seconds.');
   }
   
   function Graph(word, el, endpoint){
+    var t = new Date().getTime();
     this.word = word;
     this.target = el;
     this.flot = $.plot(
           this.target.children('.flot-container'),
-          [],
-          {}
+          [[[t, 0]], [[t, 0]]],
+          {
+            xaxis: {
+              show: false
+            },
+            yaxis: {
+              min: 0
+            }
+          }
         );
-    
     this.socket = new Socket(this, word, endpoint);
     
   }
@@ -54,7 +73,6 @@ $(document).ready(function(){
         endpoint = el.data().endpoint;
     
     var g = new Graph(word, el, endpoint);
-    
   })
   
 });
