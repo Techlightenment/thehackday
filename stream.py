bc995cdaf9666d1fcb7d93546a74db979613a145
paullawson <paul.lawson@techlightenment.com>
2012-05-31 08:33:41 +0100

    added module to stream tweets and analyse sentiment

---
+++ stream.py
@@ -0,0 +1,25 @@
+import re
+import tweetstream
+
+filename = 'wordlist.txt'
+wordmap = dict(map(lambda (w,s): (w, int(s)), [ws.strip().split('\t') for ws in open(filename)]))
+word_split = re.compile(r"\W+")
+
+def sentiment(text):
+    words = word_split.split(text.lower())
+    sentiments = [score for score in map(lambda word: wordmap.get(word, 0), words) if score]
+    if sentiments:
+        sentiment = float(sum(sentiments))
+    else:
+        sentiment = 0
+    return sentiment
+
+def run(words):
+    t = tweetstream.FilterStream('paolo_lawson', 'd0ublet0p', track=words)
+    for tweet in t:
+        text = tweet['text']
+        print text
+        print "sentiment: %s" % sentiment(text)
+
+if __name__ == '__main__':
+    run(['olympics', 'jubilee', 'euro2012', 'london2012'])
